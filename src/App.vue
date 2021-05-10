e8ed21c2e1dda20803ac6818a060db3b2563c695
Peter Knipp <peteraknipp@gmail.com>
2021-05-10 10:35:18 -0400

    Get 1-d collisions to work w/DRY code.

--- src/App.vue
+++ src/App.vue
@@ -3,35 +3,13 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to my very first Vue.js App" /> -->
     <button @click="running = !running">{{running ? "PAUSE" : "START"}}</button>
-    <div>time = {{time.toFixed(2)}} s</div>
-    <div>dtLast = {{dtLast / 1000}} s</div>
-    <div>vs = {{vs}} px/s</div>
-    <div>xs = {{xs}} px</div>
-    <div>numCol = {{numCol}}</div>
-    <div class="container" height="800px" width="1200px">
-      <div class="dot" v-bind:style="{
-        left:xs[0] + 'px',
+    <div>time = {{time.toFixed(2)}} s, numCol = {{numCol}}</div>
+    <div class="container" height="800px" v-bind:style="{width: width + 'px'}">
+      <div v-for="(x,i) of xs" :key="i" class="dot" v-bind:style="{
+        left:x + 'px',
         height: diameter + 'px',
         width: diameter + 'px',
-        transitionDuration: 1 * dtNext / 1000 + 's'
-      }"></div>
-      <div class="dot" v-bind:style="{
-        left:xs[1] + 'px',
-        height: diameter + 'px',
-        width: diameter + 'px',
-        transitionDuration: 1 * dtNext / 1000 + 's'
-      }"></div>
-      <div class="dot" v-bind:style="{
-        left:xs[2] + 'px',
-        height: diameter + 'px',
-        width: diameter + 'px',
-        transitionDuration: 1 * dtNext / 1000 + 's'
-      }"></div>
-      <div class="dot" v-bind:style="{
-        left:xs[3] + 'px',
-        height: diameter + 'px',
-        width: diameter + 'px',
-        transitionDuration: 1 * dtNext / 1000 + 's'
+        transitionDuration: dtNext / 1000 + 's'
       }"></div>
     </div>
   </div>
@@ -51,10 +29,11 @@ export default {
       dtNext: 0,
       time: 0,
       dtLast: null,
-      xs: [300, 500, 700, 900],
-      vs: [10, -20, -25, -10],
+      xs: [100, 300, 500, 700, 900, 1100],
+      vs: [200, 400, 1000, -200, 300, -70],
       diameter: 100,
       numCol: 0,
+      width: 1200,
       running: true
     }
   },
@@ -68,20 +47,33 @@ export default {
       if (this.numCol > 0) {
         this.dtLast = this.dtNext;
         let dtMin = Infinity;
-        let dt, iMin, jMin;
+        let dt, iMin, jMin, dv, dx;
         for (let i = 0; i < this.xs.length; i++) {
-          for (let j = i + 1; j < this.xs.length; j++) {
-            dt = (this.xs[j] - this.xs[i] - this.diameter) / (this.vs[i] - this.vs[j]);
-            if (dt > 0 && dt < dtMin) [iMin, jMin, dtMin] = [i, j, dt];
+          for (let j = i; j < this.xs.length; j++) {
+            if (j !== i) {
+              dv = this.vs[i] - this.vs[j];
+              dx = this.xs[j] - this.xs[i];
+            } else {
+              dv = 2 * this.vs[i];
+              dx = 2 * ((dv > 0 ? this.width : 0) - this.xs[i]);
+            }
+            if (Math.sign(dv) === Math.sign(dx)) {
+              dt = (Math.abs(dx) - this.diameter) / Math.abs(dv);
+              if (dt < dtMin)[iMin, jMin, dtMin] = [i, j, dt];
+            }
           }
         }
+        this.xs = this.xs.map((x, i) => x + this.vs[i] * dtMin);
         this.dtNext = dtMin * 1000;
-        this.xs = this.xs.map((x, i) => x + this.vs[i] * this.dtNext / 1000);
-        [this.vs[iMin], this.vs[jMin]] = [this.vs[jMin], this.vs[iMin]];
+        if (iMin !== jMin) {
+          [this.vs[iMin], this.vs[jMin]] = [this.vs[jMin], this.vs[iMin]];
+        } else {
+          this.vs[iMin] *= -1;
+        }
         this.time += this.running ? this.dtNext / 1000 : 0;
       }
       this.numCol++;
-      if (this.dtNext >= 0) this.timeout = setTimeout(this.increment, this.dtNext);
+      this.timeout = setTimeout(this.increment, this.dtNext);
     }
   },
   created() {this.increment()}

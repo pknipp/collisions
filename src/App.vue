39ae7b2ccab0e64bdc6ac7396975dd2367f671f5
Peter Knipp <peteraknipp@gmail.com>
2021-05-16 21:42:28 -0400

    Strive to make collisions work in 3d.

--- src/App.vue
+++ src/App.vue
@@ -39,19 +39,19 @@ export default {
       dt: 0,
       // units are px and px/s
       dots: [
-        {id: 1, diameter: 100, rxy: [100, 300, 100], vxy: [0, 0, 0]},
-        {id: 2, diameter: 100, rxy: [500, 300, 150], vxy: [-50, 0, 0]},
-        // {id: 3, diameter: 60, rxy: [600, 200], vxy: [-100, -90]},
-        // {id: 4, diameter: 80, rxy: [800, 500], vxy: [50, 50]},
-        // {id: 5, diameter: 100, rxy: [1100, 100], vxy: [-40, -80]},
-        // {id: 6, diameter: 120, rxy: [1200, 300], vxy: [80, -300]},
-        // {id: 7, diameter: 140, rxy: [500, 700], vxy: [-100, 200]},
-        // {id: 8, diameter: 160, rxy: [700, 700], vxy: [100, 90]},
-        // {id: 9, diameter: 180, rxy: [1100, 500], vxy: [-50, -50]},
-        // {id: 10, diameter: 200, rxy: [900, 100], vxy: [40, -80]},
+        {id: 1, diameter: 100, rxy: [100, 200, 110], vxy: [10, 5, 100]},
+        {id: 2, diameter: 100, rxy: [500, 500, 200], vxy: [200, 1, 2]},
+        {id: 3, diameter: 60, rxy: [600, 200, 500], vxy: [-100, -90, 20]},
+        {id: 4, diameter: 80, rxy: [800, 500, 200], vxy: [50, 50, -30]},
+        {id: 5, diameter: 100, rxy: [1100, 100, 100], vxy: [-40, -80, 50]},
+        {id: 6, diameter: 120, rxy: [1200, 300, 600], vxy: [80, -300, 30]},
+        {id: 7, diameter: 140, rxy: [500, 700, 100], vxy: [-100, 200, 300]},
+        {id: 8, diameter: 160, rxy: [700, 700, 300], vxy: [100, 90, 10]},
+        {id: 9, diameter: 180, rxy: [1100, 500, 500], vxy: [-50, -50, 100]},
+        {id: 10, diameter: 200, rxy: [900, 100, 600], vxy: [40, -80, -200]},
       ],
       numCol: 0,
-      dims: [250, 800],
+      dims: [1600, 800, 800],
       running: true
     }
   },
@@ -77,16 +77,38 @@ export default {
         return (-dot.vxy[1]*(dot.rxy[0]-dot.diameter/2)>-dot.vxy[0] * (dot.rxy[1]-dot.diameter/2)) ? [1, 0] : [0, 0];
       }
     },
+    walls: function(dims, dot) {
+      let [rxy, vxy] = [[...dot.rxy], [...dot.vxy]];
+      vxy.forEach((vcomp, k) => {
+        if (vcomp > 0) {
+          vxy[k] *= -1;
+          rxy[k] = dims[k] - rxy[k];
+        }
+      })
+      // 1st: look at x = 0 plane.
+      let t = -(rxy[0] - dot.diameter / 2) / vxy[0];
+      let y = rxy[1] + vxy[1] * t;
+      let z = rxy[2] + vxy[2] * t;
+      console.log("y = ", y)
+      if (y >= 0 && z >= 0) return {index: 0, t};
+      // 2nd: look at y = 0 plane.
+      t = -(rxy[1] - dot.diameter / 2) / vxy[1];
+      let x = rxy[0] + vxy[0] * t;
+      z = rxy[2] + vxy[2] * t;
+      if (x >= 0 && z >= 0) return {index: 1, t};
+      // By process of elimination, it must be z = 0 plane.
+      return {index: 2, t: -(rxy[2] - dot.diameter) / vxy[2]};
+    },
     increment: function () {
       // "numCol" means the number of the next collision, w/1-based counting
       if (this.numCol) {
         let dtMin = Infinity;
-        let dv, dr, wallIndex, wallIndexMin, iMin, jMin;
+        let dv, dr, result, iMin, jMin, indexMin;
         for (let i = 0; i < this.dots.length; i++) {
           for (let j = i; j < this.dots.length; j++) {
             let dt = Infinity;
+            let doti = this.dots[i];
             if (j !== i) {
-              let doti = this.dots[i];
               let dotj = this.dots[j];
               // relative velocity and position (vectors) of two dots
               dv = doti.vxy.map((vcomp, k) => vcomp - dotj.vxy[k]);
@@ -97,28 +119,33 @@ export default {
               let c = dr.reduce((dr2, comp) => dr2 + comp ** 2, 0) - totRadius * totRadius;
               let b = 2 * dr.reduce((dot, comp, k) => dot + comp * dv[k], 0);
               let disc = b * b - 4 * a * c;
-              // 1st test if particles are "closing the gap", and then whether collision'll occur
+              // 1st test if 2 particles are "closing the gap", and then whether collision'll occur
               if (b <= 0 && disc >= 0) dt = 2 * c / (-b + Math.sqrt(disc));
             } else {
-              wallIndex = this.whichWall(this.dots[i]);
-              if (wallIndex[1]) {
-                // reflects from top & bottom walls
-                dt = (this.dims[wallIndex[0]] - this.dots[i].diameter / 2 - this.dots[i].rxy[wallIndex[0]]) / this.dots[i].vxy[wallIndex[0]];
-              } else {
-                // reflects from right & left walls
-                dt = -(this.dots[i].rxy[wallIndex[0]] - this.dots[i].diameter / 2) / this.dots[i].vxy[wallIndex[0]];
-              }
+              result = this.walls(this.dims, doti);
+              dt = result.t;
+              // wallIndex = this.whichWall(this.dots[i]);
+              // if (wallIndex[1]) {
+              //   // reflects from top & bottom walls
+              //   dt = (this.dims[wallIndex[0]] - this.dots[i].diameter / 2 - this.dots[i].rxy[wallIndex[0]]) / this.dots[i].vxy[wallIndex[0]];
+              // } else {
+              //   // reflects from right & left walls
+              //   dt = -(this.dots[i].rxy[wallIndex[0]] - this.dots[i].diameter / 2) / this.dots[i].vxy[wallIndex[0]];
+              // }
             }
             // update the details for the collision which'll occur next
-            if (dt < dtMin) [dtMin, iMin, jMin, wallIndexMin] = [dt, i, j, [...wallIndex]];
+            if (dt < dtMin) [dtMin, iMin, jMin, indexMin] = [dt, i, j, result.index];
           }
         }
         // Change each dot's coordinates until the moment of the next collision.
         this.dots.forEach((dot, i) => dot.rxy.forEach((comp, j) => this.dots[i].rxy[j] += dot.vxy[j] * dtMin));
+        this.dots.forEach((dot, i) => console.log("i/z/indexMin = ", i, dot.rxy[2], indexMin));
         this.dt = dtMin;
+        // The collision changes one or more components of one or two dots.
         if (iMin === jMin) {
           // If the collision was with a wall, negate the appropriate component of the relevant dot's velocity.
-          this.dots[iMin].vxy[wallIndexMin[0]] *= -1;
+          // this.dots[iMin].vxy[wallIndexMin[0]] *= -1;
+          this.dots[iMin].vxy[indexMin] *= -1;
         } else {
           // If the collision was between two dots, the procedure is more complicated.
           let [doti, dotj] = [this.dots[iMin], this.dots[jMin]];
@@ -138,16 +165,12 @@ export default {
           let dp = dr.map(comp => 2 * comp * dot / dr2);
 
           // transfer momentum from one object to the other:
-          console.log(dp);
-          console.log(vi, vj);
           vi = vi.map((comp, k) => comp - dp[k] / massi);
           vj = vj.map((comp, k) => comp + dp[k] / massj);
-          console.log(vi, vj);
 
           // Shift back to lab frame.
           doti.vxy = vi.map((vcomp, k) => vcomp + v_cm[k]);
           dotj.vxy = vj.map((vcomp, k) => vcomp + v_cm[k]);
-          console.log(doti.vxy, dotj.vxy);
         }
         // Advance the clock.
         this.time += this.running ? this.dt : 0;
